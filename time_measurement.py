fdfeff5c5ca342d3124bb5b8162f7bc5da6e05c0
Nicolas Vincent <nicolas.vincent100@gmail.com>
2020-02-02 13:45:11 +0100

    Images svg des tests

--- time_measurement.py
+++ time_measurement.py
@@ -8,12 +8,14 @@ time_measurement.py : //
 
 
 from time import perf_counter_ns
+import os.path
+
 from main import trouve_inclusions_sorted, trouve_inclusions
 from tycat import read_instance
 from generate_from_examples import generator
 
 
-def bench(preparation, a_mesurer, repetitions=10):
+def bench(preparation, a_mesurer, repetitions=30):
     temps = []
     for _ in range(repetitions):
         entree = preparation
@@ -26,16 +28,19 @@ def bench(preparation, a_mesurer, repetitions=10):
 
 def perf_test(a_mesurer):
     # augmenter le range de number si besoin, descendre à 6 pour avoir des résultats corrects et un temps réduit d'exécution
-    for number in range(1, 7):
-        generator(number=number)  # si les fichiers ne sont pas déjà générés
-        preparation = read_instance(f"generated_from_examples_{number}.poly")
+    for number in range(1, 6):
+        filename = f"generated_from_examples_{number}.poly"
+        if not os.path.exists(filename):
+            print("Files generation...")
+            generator(number=number)
+        preparation = read_instance(filename)
         for temps in bench(preparation, a_mesurer):
             print(number, temps)
 
 
 def main():
-    perf_test(trouve_inclusions)
-    # perf_test(trouve_inclusions_sorted)
+    # perf_test(trouve_inclusions)
+    perf_test(trouve_inclusions_sorted)
 
 
 if __name__=="__main__":
